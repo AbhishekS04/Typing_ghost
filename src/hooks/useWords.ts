d623bfdf6f352767c471a480bb65bae1c20be1aa
ABHISHEK SINGH <Abhishek23main@gmail.com>
2024-09-11 23:15:55 +0530

    useWords.ts

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -1,18 +1,19 @@
-import Chance from "chance";  // Import Chance
 import { useCallback, useState } from "react";
+import { faker } from '@faker-js/faker';
 
-const chance = new Chance();  // Initialize Chance instance
-
-const generateWords = (count: number) => {
-  return chance.sentence({ words: count }).toLowerCase();  // Use chance for word generation
+// Generate a coherent English string with a specified number of words
+const generateWords = (wordCount: number, useSentences = false) => {
+  let text = useSentences ? faker.lorem.sentences(2) : faker.word.words(wordCount); // Use sentences or words
+  const wordsArray = text.split(' '); // Split into individual words
+  return wordsArray.slice(0, wordCount).join(' ').replace(/[^\w\s]|_/g, "").toLowerCase(); // Remove punctuation and lowercase
 };
 
-const useWords = (count: number) => {
-  const [words, setWords] = useState<string>(generateWords(count));
+const useWords = (count: number, useSentences = false) => {
+  const [words, setWords] = useState<string>(generateWords(count, useSentences));
 
   const updateWords = useCallback(() => {
-    setWords(generateWords(count));
-  }, [count]);
+    setWords(generateWords(count, useSentences));
+  }, [count, useSentences]);
 
   return { words, updateWords };
 };
